658fd177c74c41df0d41a515212acdc1445c1b7d
盧仁杰 <Congnarldria@gmail.com>
2020-12-11 16:45:57 +0800

    new method

--- ATMTCommonLib/ATMTVirtualMouse.cs
+++ ATMTCommonLib/ATMTVirtualMouse.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using System.Windows.Input;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace ATMTCommonLib
 {
@@ -44,41 +45,79 @@ namespace ATMTCommonLib
 
         [DllImport("user32.dll", EntryPoint = "SendMessage")]
         private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32.dll", EntryPoint = "ClipCursor")]
+        private static extern long ClipCursor(RECT rec);
+        [DllImport("user32.dll", EntryPoint = "ClipCursor")]
+        private static extern long ClipCursor(Rectangle rec);
+        [DllImport("user32.dll", EntryPoint = "ShowCursor")]
+        private static extern long ShowCursor(long show);
+        /// <summary>
+        /// in screen coordinates
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        public static extern long SetCursorPos(long x, long y);
+        [DllImport("user32.dll", EntryPoint = "GetWindowRect")]
+        private static extern long GetWindowRect(IntPtr hdl, out RECT rec);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
 
+        }
         #region VirtualMouse
         private const UInt32 MouseEventLeftDown = 0x0002;
         private const UInt32 MouseEventLeftUp = 0x0004;
         private const UInt32 MouseEventRightDown = 0x0008;
         private const UInt32 MouseEventRightUp = 0x0010;
 
-        public static void SendLeftClick(UInt32 posX, UInt32 posY)
+        public static void LeftClick(UInt32 posX, UInt32 posY)
         {
             mouse_event(MouseEventLeftDown, posX, posY, 0, new System.IntPtr());
             mouse_event(MouseEventLeftUp, posX, posY, 0, new System.IntPtr());
         }
-        public static void SendRightClick(UInt32 posX, UInt32 posY)
+        public static void RightClick(UInt32 posX, UInt32 posY)
         {
             mouse_event(MouseEventRightDown, posX, posY, 0, new System.IntPtr());
             mouse_event(MouseEventRightUp, posX, posY, 0, new System.IntPtr());
         }
+        public static bool LockCursor(Control ctl)
+        {
+            RECT rec = new RECT();
+            long res;
+            GetWindowRect(ctl.Handle, out rec);
+            res = ClipCursor(rec);
+            if (res == 1) return true;
+            else
+                return false;
+        }
+        public static void UnLockCursor()
+        {
+            ClipCursor(Screen.PrimaryScreen.Bounds);
+        }
         #endregion
-        private const int KEYEVENTF_KEYUP = 0x02;
-        private const int KEYEVENTF_KEYDOWN = 0x00;
+        private const int KEYEVENTF_KEYUP = 0x0002;
+        private const int KEYEVENTF_KEYDOWN = 0x0000;
         #region KeyBoard
-        public static void KeyClick(Keys key)
-        { 
-            //第三個參數 0 按下 2 鬆開
+        public static void KeyClick(Control ctl ,  Keys key)
+        {
+            ctl.Focus();
             keybd_event((byte)key, 0x45, KEYEVENTF_KEYDOWN, 0);
             keybd_event((byte)key, 0x45, KEYEVENTF_KEYUP, 0);
         }
-        public static void KeyDown(Keys key)
+        public static void KeyDown(Control ctl ,  Keys key)
         {
-            //第三個參數 0 按下 2 鬆開
+            ctl.Focus();
             keybd_event((byte)key, 0x45, KEYEVENTF_KEYDOWN, 0);
         }
-        public static void KeyUp(Keys key)
+        public static void KeyUp(Control ctl , Keys key)
         {
-            //第三個參數 0 按下 2 鬆開
+            ctl.Focus();
             keybd_event((byte)key, 0x45, KEYEVENTF_KEYUP, 0);
         }
         #endregion
@@ -92,15 +131,26 @@ namespace ATMTCommonLib
                 IntPtr hwnd = poc.Handle;
                 SetForegroundWindow(hwnd);//設置App的焦點
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 LogMgr.SendLog(e.Message, e);
             }
         }
         #endregion
 
+        //        SendMessage(Button.Handle, WM_LBUTTONDOWN, 0, 0); //鼠標左鍵按下 
 
-        //                        KEY    CODE       KEY     CODE          KEY         CODE          KEY                        KEYCODE
+        //        SendMessage(Button.Handle, WM_LBUTTONUP, 0, 0); //鼠標左鍵抬起
+
+        //        SendMessage(Edit.Handle, WM_SETTEXT, 255, Integer(PChar('abc'))); //傳遞文本
+
+        //          SendMessage(Edit.Handle, WM_Char, Wparam('Q'), 2); //傳遞字符
+
+        //          SendMessage(Button.Handle, BM_SETSTYLE, BS_RADIOBUTTON, 1); //改變Button風格
+
+        //        SendMessage(ComboBox.Handle, CB_SETDROPPEDWIDTH, 300, 0); //改變CBDownWidth
+
+        //                        KEY    CODE       KEY     CODE          KEY         CODE          KEY                CODE
 
         //                        A　　　65　　   0 　　96 　　　　F1 　　112 　　Backspace 　　　8 
         //　　　　　　B　　　66　　   1　　 97 　　　　F2 　　113　　 Tab 　　　　　　9 
@@ -117,10 +167,10 @@ namespace ATMTCommonLib
         //                        M　　　77 　　　Enter 108　　　　-- 　　--　　　End 　　　　　　35 
         //　　　　　　N　　　78 　　　-　　 109　　　　-- 　　-- 　　　Home　　　　　　36 
         //　　　　　　O　　　79 　　　. 　　110　　　　--　　 -- 　　 　Left Arrow　　　37 
-        //                         P　　　80 　　　/ 　　111　　　　--　　 -- 　　 　Up Arrow　　　　38 
-        //                          Q　　　81 　　　-- 　　--　　　 　--　　 -- 　　 Right Arrow 　　39 
-        //                          R　　　82 　　　-- 　　--　　　　--　　 -- 　　   Down Arrow 　　 40 
-        //                          S　　　83 　　　-- 　　--　　　　　-- 　　-- 　　 　Insert 　　　　 45 
+        //                        P　　　80 　　　/ 　　111　　　　--　　 -- 　　 　Up Arrow　　　　38 
+        //                        Q　　　81 　　　-- 　　--　　　 　--　　 -- 　　 Right Arrow 　　39 
+        //                        R　　　82 　　　-- 　　--　　　　--　　 -- 　　   Down Arrow 　　 40 
+        //                        S　　　83 　　　-- 　　--　　　　　-- 　　-- 　　 　Insert 　　　　 45 
         //　　　　　　T　　　84 　　　-- 　　--　　　　　--　　 -- 　　 　Delete 　　　　 46 
         //　　　　　　U　　　85 　　　-- 　　--　　　 　-- 　　-- 　　 　Help 　　　　　 47 
         //　　　　　　V　　　86 　　　--　　 --　　　　-- 　　-- 　　 　Num Lock 　　　 144 
