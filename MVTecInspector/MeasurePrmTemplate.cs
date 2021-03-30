60c802c841ac6d447f79d45dff378d659aac394e
盧仁杰 <Congnarldria@gmail.com>
2021-03-30 08:57:36 +0800

    ....

--- MVTecInspector/MeasurePrmTemplate.cs
+++ MVTecInspector/MeasurePrmTemplate.cs
@@ -14,7 +14,7 @@ namespace CommonInspector
     }
     public class Tc
     {
-        private static bool IsValid(HObject obj)
+        public static bool IsValid(HObject obj)
         {
             if (obj == null) return false;
             if (!obj.IsInitialized())
@@ -25,6 +25,7 @@ namespace CommonInspector
         }
         public static bool IsTupleNotEmptry(HTuple val)
         {
+            if (val == null) return false;
             HOperatorSet.TupleLength(val, out HTuple Count);
             if (Count > 0)
                 return true;
@@ -37,8 +38,8 @@ namespace CommonInspector
                 obj.Dispose();
         }
     }
-    public enum EmTransition { all, positive, negative }
-    public enum EmSelect { all, firse, last }
+    public enum EmTransition { all, positive, negative, separate }
+    public enum EmSelect { all, first, last, separate }
     [Serializable]
     public class TMeasurePosPrm
     {
@@ -47,9 +48,12 @@ namespace CommonInspector
 
         }
         public EmTransition Transition { get; set; } = EmTransition.all;
-        public EmSelect Select { get; set; } = EmSelect.firse;
+        public EmSelect Select { get; set; } = EmSelect.first;
+        public string MeasureTransition { get; set; } = "all";
+        public string MeasureSelect { get; set; } = "first";
         public int Threashold { get; set; } = 30;
         public int Sigma { get; set; } = 1;
+        public double Amplitude { get; set; } = 30;
         public byte[] MeasureHandleByteArray { get; set; } = null;
         [XmlIgnore]
         public HTuple MeasureHandle { get; set; } = null;
@@ -86,7 +90,22 @@ namespace CommonInspector
     {
         public TAddMetrologyObjectLineMeasurePrm()
         {
-
+        }
+        public void CreateMetrologyModel()
+        {
+            if (Tc.IsTupleNotEmptry(MetrologyHandle))
+            {
+                HOperatorSet.ClearMetrologyModel(MetrologyHandle);
+            }
+            HOperatorSet.CreateMetrologyModel(out HTuple MetrologyModel);
+            MetrologyHandle = MetrologyModel;
+        }
+        public void SetPrm()
+        {
+            HOperatorSet.SetMetrologyModelParam(MetrologyHandle, EmMetrologyPrm.num_measures.ToString(), NumMeasures);
+            HOperatorSet.SetMetrologyModelParam(MetrologyHandle, EmMetrologyPrm.measure_transition.ToString(), MeasureTransition);
+            HOperatorSet.SetMetrologyModelParam(MetrologyHandle, EmMetrologyPrm.measure_select.ToString(), MeasureSelect);
+            HOperatorSet.SetMetrologyModelParam(MetrologyHandle, EmMetrologyPrm.min_score.ToString(), MinScore);
         }
         [XmlIgnore]
         public HTuple MetrologyHandle { get; set; } = null;
@@ -94,19 +113,33 @@ namespace CommonInspector
         public double ColumnBegin { get; set; } = 0;
         public double RowEnd { get; set; } = 1;
         public double ColumnEnd { get; set; } = 1;
-        public double MeasureLength1 { get; set; } = 20;
-        public double MeasureLength2 { get; set; } = 5;
         public double MeasureSigma { get; set; } = 1;
         public double MeasureThreshold { get; set; } = 30;
+        public int NumMeasures { get; set; } = 100;
+        public double MeasureLength1 { get; set; } = 30;
+        public double MeasureLength2 { get; set; } = 5;
+
+        public string MeasureTransition { get; set; } = "positive";
+        public string MeasureSelect { get; set; } = "all";
+        public double MinScore { get; set; } = 0.6;
+        [XmlIgnore]
+        public double LineRowBegin { get; set; } = 0;
+        [XmlIgnore]
+        public double LineRowEnd { get; set; } = 0;
+        [XmlIgnore]
+        public double LineColBegin { get; set; } = 10;
+        [XmlIgnore]
+        public double LineColEnd { get; set; } = 10;
+
         public EmMetrologyPrm MetrologyPrm = EmMetrologyPrm.distance_threshold;
         public byte[] MetrologyandleByteArray { get; set; } = null;
         [XmlIgnore]
-        public HTuple MeasureHandle { get; set; } = null;
+        public HObject LineObj = null;
         public void SaveMetrologyPrm()
         {
-            if (MeasureHandle != null)
+            if (MetrologyHandle != null)
             {
-                HOperatorSet.SerializeMeasure(MeasureHandle, out HTuple SeriallizeItem);
+                HOperatorSet.SerializeMeasure(MetrologyHandle, out HTuple SeriallizeItem);
                 HSerializedItem item = new HSerializedItem(SeriallizeItem.H);
                 MetrologyandleByteArray = item;
                 item.Dispose();
@@ -125,7 +158,7 @@ namespace CommonInspector
                         item.CreateSerializedItemPtr((IntPtr)p, bb.Length, "true");
                     }
                     HOperatorSet.DeserializeMeasure(item, out HTuple m_Handle);
-                    MeasureHandle = m_Handle;
+                    MetrologyHandle = m_Handle;
                 }
             }
         }
